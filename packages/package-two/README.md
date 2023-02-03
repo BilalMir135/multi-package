e59292df22408b7ea67aa27b5ab4a12fd0a96297
BilalMir135 <bilalmir135@gmail.com>
2023-02-04 01:27:54 +0500

    feat(packages): initial functions

--- packages/package-two/README.md
+++ packages/package-two/README.md
@@ -1,1 +1,4 @@
 # @bilalmir135/package-two
+
+Complex Expressions
+Publish on [npm](https://www.npmjs.com/package/@bilalmir135/package-two)
--- packages/package-two/src/index.ts
+++ packages/package-two/src/index.ts
@@ -1,1 +1,11 @@
-console.log('Package One')
+import BasicExpression from '@bilalmir135/package-one'
+
+function sumMul(num_a: number, num_b: number, num_c: number) {
+  return BasicExpression.sum(num_a, num_b) * num_c
+}
+
+function subMul(num_a: number, num_b: number, num_c: number) {
+  return BasicExpression.sub(num_a, num_b) * num_c
+}
+
+export default { sumMul, subMul }
